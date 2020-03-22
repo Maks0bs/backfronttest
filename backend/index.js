b492dc5db8e61f111a943d33f046c314b50c2f28
maks0bs <maksthebro@173gmail.com>
2020-03-22 20:27:03 +0100

    trying running only backed for deply with sending frontend files via express

--- backend/index.js
+++ backend/index.js
@@ -8,4 +8,12 @@ app.get('/api/message', (req, res) => {
 	})
 })
 
+if (process.env.NODE_ENV === 'production') {
+	//set static folder
+	app.use(express.static('../frontend/build'))
+	app.get('*', (req, res) => {
+		res.sendFile('index.html', {root: path.join(__dirname, '../frontend/build')});
+	})
+}
+
 app.listen(process.env.PORT || 3000);
