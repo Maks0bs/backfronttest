1d0cf9ebb4be24985ba32d33050a604f1a6f83b6
maks0bs <maksthebro@173gmail.com>
2020-03-23 12:24:10 +0100

    trying subdomain

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,20 @@
 let express = require('express');
 let cors = require('cors');
 let path = require('path');
+let subdomain = require('express-subdomain');
 
 let app = express();
 
 app.use(cors())
 
-app.get('/api/message', (req, res) => {
+let router = express.Router();
+
+router.get('/message', (req, res) => {
 	res.json({
 		message: 'it works'
 	})
 })
+
+app.use(subdomain('api', router));
 
 app.listen(process.env.PORT || 3000);
